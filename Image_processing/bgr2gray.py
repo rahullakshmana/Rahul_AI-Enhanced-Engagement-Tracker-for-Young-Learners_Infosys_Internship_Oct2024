e27471fd4f1c73d4602d043775e2131b272dda10
rahullakshmana <rahullakshmana004@gmail.com>
2024-11-14 16:44:07 +0530

    Update bgr2gray.py

--- Image_processing/bgr2gray.py
+++ Image_processing/bgr2gray.py
@@ -1,8 +1,8 @@
 import cv2
 
-image = cv2.imread(r'C:\Users\RAHUL\Desktop\Image_processing\image2.jpg')
+image = cv2.imread(r'C:\Users\RAHUL\Desktop\infosys_springboard\Image_processing\image2.jpg')
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite(r'C:\Users\RAHUL\Desktop\Image_processing\image1.jpg', gray_image)
+cv2.imwrite(r'C:\Users\RAHUL\Desktop\Image_processing\gray.jpg', gray_image)
 cv2.imshow('Grayscale Image', gray_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
